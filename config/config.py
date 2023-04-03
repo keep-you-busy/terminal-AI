2a6c45dad40ba5fe7923edc20f8fc1d19e4300f2
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-04-03 21:21:53 +0700

    Changes in logic of configure

--- config/config.py
+++ config/config.py
@@ -1,9 +1,12 @@
-
 from dynaconf import Dynaconf
 
 settings = Dynaconf(
     root_path='config/',
+    core_loaders=['YAML'],
+    default_env='default',
     envvar_prefix='TERMINAL_AI',
-    settings_files=['settings.yaml', '.secrets.yaml'],
+    settings_files=['settings.yaml', '.secrets.yaml', 'customization.yaml'],
+    # environments=['default', 'development', 'production'],
     yaml_loader='safe_load',
+    load_dotenv=False
 )
